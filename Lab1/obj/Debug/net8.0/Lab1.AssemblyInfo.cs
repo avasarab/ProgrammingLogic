0702f46c40eb14dd1cc732dd2d630e433cdb875d
avasarab <14avasarab@gmail.com>
2025-03-19 01:51:22 +0000

    modified lab 1

--- Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
+++ Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dddfdf9f4e3baf18caecc1150b56a3b3ec9a1b17")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+731568b1ec28215cc79990da960ea801051dc380")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
