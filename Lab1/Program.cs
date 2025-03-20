8c518e1ba3df7e6bc542593748d27dad06b3dcc9
avasarab <14avasarab@gmail.com>
2025-03-20 19:44:27 -0400

    Update Program.cs

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -5,7 +5,20 @@ using System;
     static void Main(string[] args)
     {
       // Step 1
- Console.WriteLine("Allison Vasarab Programming Logic");
-     }
-    }
+      Console.WriteLine("Allison Vasarab Programming Logic");
+      
+      // Step 2
+      /* Allison Vasarab 
+      IT-1050 - Lab 1 */
+      
+      // Step 3
+      int myNum = 23;
+      Console.WriteLine(myNum);
+      string name = "CSS";
+      Console.WriteLine(name);
+      double myDoubleNum = 0.00D;
+      Console.WriteLine(myDoubleNum);
+      bool myBool = false;
+      Console.WriteLine(bool);
+  }
   }
