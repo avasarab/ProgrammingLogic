d467d5b51c8e22f1b10fbd288751b12e86ba1264
avasarab <14avasarab@gmail.com>
2025-03-22 04:35:29 +0000

    Added Lab1 console app

--- Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
+++ Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+731568b1ec28215cc79990da960ea801051dc380")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8576b312740161e02e71c0cfb7f3067e6380e380")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
