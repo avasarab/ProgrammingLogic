efc988210fbb1e0f67edc1e0c87346f22d9ab11b
avasarab <14avasarab@gmail.com>
2025-03-29 17:18:42 +0000

    Added Lab1 console app

---
+++ Lab1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ProgrammingLogic
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // Step 1
+      Console.WriteLine("Allison Vasarab Programming Logic");
+     
+      // Step 2
+      /* Allison Vasarab
+      IT-1050 - Lab 1 */
+     
+      // Step 3
+      int myNum = 23;
+      Console.WriteLine(myNum);
+      string name = "CSS";
+      Console.WriteLine(name);
+      double myDoubleNum = 0.00D;
+      Console.WriteLine(myDoubleNum);
+      bool myBool = false;
+      Console.WriteLine(myBool);
+
+      // Step 4
+      const string collegeName = "Cuyahoga Community College";
+      Console.WriteLine(collegeName);
+
+    }
+  }
+}
