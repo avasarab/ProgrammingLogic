a5ef6e1fe54ce149f941d1648e14d2451a2af543
avasarab <14avasarab@gmail.com>
2025-03-29 17:28:03 +0000

    added lab2

---
+++ Lab2/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Lab2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+int myInt = 23;
+    Console.WriteLine(myInt);
+double myDoubleNum = 8.9999D;
+    Console.WriteLine(myDoubleNum);
+float myFloat = 9.99999999F;
+    Console.WriteLine(myFloat);
+char myChar = 'A';
+    Console.WriteLine(myChar);
+bool myBool = true;
+    Console.WriteLine(myBool);
+string myString = "Howdy";
+    Console.WriteLine(myString);
+        
+    }
+}
