82567c0c315cbf62dac2493cf6082829e259f44d
avasarab <14avasarab@gmail.com>
2025-04-15 02:23:50 +0000

    today

--- Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
+++ Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a804d26dd6e5543eddde3fb28fbe05a35680f65b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+660f5bc544c39cd42dc5ba2233f532a3c0db2271")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Lab4/Program.cs
@@ -0,0 +1,66 @@
+﻿using System.Globalization;
+
+namespace Lab4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+// Problem 1: Simple For Loop
+
+for (int x = 1; x <= 10; x++) 
+{
+  Console.WriteLine(x);
+}
+
+// Problem 2: Even Numbers from 1 to 20 (Using Modulus Operator)
+
+for (int i = 1; i <= 20; i++)
+if (i % 2 == 0)
+{
+    Console.WriteLine(i);
+}
+
+// Problem 3: While Loop Countdown
+
+int number = 5;
+while (number >= 1) 
+{
+  Console.WriteLine(number);
+  number--;
+}
+
+// Problem 4: Do/While User Input
+
+int numberAgain = 0;
+do 
+        {
+            Console.Write("Enter a number greater than 100: ");
+        } while (!int.TryParse(Console.ReadLine(), out numberAgain) || numberAgain <= 100);
+
+        Console.WriteLine($"Valid number entered: {numberAgain}");
+
+// Problem 5: While Loop – Multiples of 10 from 10 to 1000
+
+int whileLoop = 10;
+while (whileLoop <= 1000) 
+{
+  Console.WriteLine(whileLoop);
+  whileLoop +=10;
+}
+
+// Problem 6: Pattern Printer (Using Nested Loops)
+
+for (int triangleList = 1; triangleList <= 10; triangleList++) 
+{ 
+  for (int j = 1; j <= triangleList; j++) 
+  {
+    Console.Write("*");
+  }
+  Console.WriteLine();
+}
+    }
+}
+    
